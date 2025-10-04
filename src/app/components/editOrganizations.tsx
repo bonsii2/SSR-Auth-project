ae6f6347cdb06560d3959670377d30d3e394c675
bonsii2 <bonsadaba8@gmail.com>
2025-10-04 22:48:39 +0300

    i fix the error of edit organization

--- src/app/components/editOrganizations.tsx
+++ src/app/components/editOrganizations.tsx
@@ -5,34 +5,61 @@ import { supabase } from "../hooks/useSupabase"
 export default function EditProfile({orgId}: {orgId: string}){
  const [name, setName] = useState('')
  const [Phone, setPhone] = useState('')
- const [location, setLocation] = useState('')
-//  const [logo, SetLogo] = useState('')
+ const [location, setLocation] = useState('');
+ const [Logo, SetLogo] = useState<File | null>(null);
+ const [logoUrl, setLogoUrl] = useState('')
+    
  
 useEffect(() =>{
    const fetchOrganization = async () => {
-      const {data, error} = await supabase
-      .from('organization')
-      .select('name, location, phone')
-      .eq('id', orgId)
-      .single()
+      const { data, error } = await supabase
+        .from("organization")
+        .select("*")
+        .eq("id", orgId)
+        .single();
 
       if(data){
         setName(data.name);
         setLocation(data.location);
         setPhone(data.phone);
+        SetLogo(data.logo);
       }
    };
     fetchOrganization();
 }, [orgId])
-
+  let finalUrl = logoUrl;
 const handleUpdate = async () =>{
-    const {error} = await supabase
-    .from('organization')
-    .update({name, Phone, location})
 
+
+  if(Logo){
+    const fileExt = Logo.name.split('.').pop();
+    const fileName = `${Date.now()}_${fileExt}`;
+    const {data, error} = await supabase.storage
+    .from('logos')
+    .upload(fileName, Logo);
+
+    if(!error){
+      const {data: {publicUrl}} = supabase.storage
+      .from('logos')
+      .getPublicUrl(fileName);
+
+      finalUrl = publicUrl;
+    }
+
+  }
+
+
+    const { error } = await supabase
+      .from("organization")
+      .update({ name, phone: Phone, location, logo: finalUrl })
+      .eq('id', orgId)
+
+
+    
     if(error){
-        console.log('update faield' + error.message)
+        console.log('update faield' + error.message);
     }else{
+        alert('updated succesfuly')
         console.log('updated seccussfully');
     }
 
@@ -43,9 +70,14 @@ const handleUpdate = async () =>{
    <input type="text" name="name" value={name} onChange={(e) => setName(e.target.value)} />
    <input type="text" name="location" value={location} onChange={(e) => setLocation(e.target.value)} />
    <input type="tel" name="phone" value={Phone} onChange={(e) => setPhone(e.target.value)} />
-   {/* <input type="file" name="logo" value={logo} onChange={(e) => SetLogo(e.target.value)} /> */}
+   <input type="file" name="logo" accept="image/*" onChange={(e) => SetLogo(e.target.files?.[0] || null)} />
 
- <button onClick={handleUpdate}>update</button>
+   {Logo && (
+    <img src={Logo} alt="logo of organization" className="w-35 h-35" />
+   )
+   }
+
+ <button  onClick={handleUpdate}>update</button>
 
 
         
--- src/app/organizations/edit/[id]/page.tsx
+++ src/app/organizations/edit/[id]/page.tsx
@@ -2,9 +2,11 @@ import EditOrganization from '@/app/components/editOrganizations'
 
 
 
-export default async function Page({params} : {params: {id: string}}){
-    const orgId = params.id;
+export default async function Page({params} : {params: Promise<{id: string}>}){
+    const {id} =  await params;
+    const orgId = id;
     if(!orgId) return <p>loading...</p>
 
     return <EditOrganization orgId={orgId} />;
 }
+
